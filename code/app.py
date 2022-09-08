f3d2a3516e7b3572b351da7e8b5bd31f5edc1bb0
Klaus <klehmann@fen.uchile.cl>
2022-09-08 13:51:58 -0400

    avance en documentacion

--- code/app.py
+++ code/app.py
@@ -3,8 +3,25 @@ from flask import  request
 import pandas as pd
 from flask import jsonify
 import os
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 app = Flask(__name__)
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+
 
 @app.route('/')
 def home():
